a34c651c3ef856e649c1648ff658b14fa376dcdb
HiperEmmanuel <hiperemmanuel@gmail.com>
2017-11-15 17:39:00 -0700

    Se agrego nueva pagina
    Ahora ya esta instaladas las apis de google maps y el gps, hace falta hacerlas funcionar.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RutaPage } from '../ruta/ruta';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+calcular(){
+
+    this.navCtrl.push(RutaPage);
+}
+ 
 
   constructor(public navCtrl: NavController) {
 
